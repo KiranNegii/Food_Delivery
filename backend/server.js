4cf5f285fb22a8b67d535ed8e4771428a5a571d1
KIRAN NEGI <163193047+KiranNegii@users.noreply.github.com>
2024-05-01 14:47:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import app from "./app.js";
 import dotenv from 'dotenv';
 dotenv.config();
+const PORT = process.env.PORT ||4000;
 
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
 })
