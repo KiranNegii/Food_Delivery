dfb982112c6c39d55f745880d953c48230c99317
KIRAN NEGI <163193047+KiranNegii@users.noreply.github.com>
2024-05-01 15:08:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config.env" });
 
 app.use(
   cors({
-    origin: 'https://main--kiran-fooddelivery.netlify.app/',
+    origin: 'https://main--kiran-fooddelivery.netlify.app',
     methods: "POST",
     credentials: true,
   })
